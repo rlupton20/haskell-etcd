9e65182c81273ccc953e3cee8525050ead35ea98
Richard Lupton <richard.lupton@gmail.com>
2017-03-19 20:35:06 +0000

    Adding basic JSON parsers
    Can extract value, previous value, and pairs of values.

--- src/Database/Etcd/JSON.hs
+++ src/Database/Etcd/JSON.hs
@@ -1,5 +1,8 @@
 module Database.Etcd.JSON (
-EtcdVersion(..) ) where
+  EtcdVersion(..)
+, Value(..)
+, PreviousValue(..)
+, Pair(..)) where
 
 import Data.Text (Text)
 import qualified Data.Aeson as A
@@ -8,8 +11,28 @@ import Data.Aeson ((.:))
 import Lib.Prelude
 
 data EtcdVersion = EtcdVersion { serverVersion :: Text
-                               , clusterVersion :: Text } deriving (Show)
+                               , clusterVersion :: Text } deriving (Show, Eq)
 
 instance A.FromJSON EtcdVersion where
-  parseJSON (A.Object v) = EtcdVersion <$> v .: "etcdserver" <*> v .: "etcdcluster"
+  parseJSON (A.Object v) = EtcdVersion <$>
+    v .: "etcdserver" <*>
+    v .: "etcdcluster"
   parseJSON _ = empty
+
+newtype Value a = Value a deriving (Show, Eq)
+
+instance (A.FromJSON a) => A.FromJSON (Value a) where
+  parseJSON (A.Object v) = Value <$> (v .: "node" >>= (.: "value"))
+  parseJSON _ = empty
+
+newtype PreviousValue a = PreviousValue a deriving (Eq, Show)
+
+instance (A.FromJSON a) => A.FromJSON (PreviousValue a) where
+  parseJSON (A.Object v) = PreviousValue <$>
+    (v .: "prevNode" >>= (.: "value"))
+  parseJSON _ = empty
+
+data Pair a b = Pair {-# UNPACK #-} !a {-# UNPACK #-} !b deriving (Eq, Show)
+
+instance (A.FromJSON a, A.FromJSON b) => A.FromJSON (Pair a b) where
+  parseJSON o = Pair <$> A.parseJSON o <*> A.parseJSON o
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Test.Framework as TF
+import Test.Framework.Providers.HUnit (hUnitTestToTests)
+import Test.HUnit as HU
+import Test.HUnit ((~:),(@=?))
+
 import Protolude
+import Database.Etcd.JSON
+import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.Aeson as A
+import Data.Text (Text)
 
 main :: IO ()
-main = putStrLn ("Test suite not yet implemented" :: Text)
+main = defaultMain tests
+
+tests :: [ TF.Test ]
+tests = [ jsonParserTests ]
+
+jsonParserTests :: TF.Test
+jsonParserTests = testGroup "JSON parser tests" . hUnitTestToTests $
+  HU.TestList [ canParseValueFromResponse
+              , canParsePreviousNodeValue
+              , canExtractPairsOfJSONValues ]
+
+canParseValueFromResponse :: HU.Test
+canParseValueFromResponse = "Can obtain the value of a node from response" ~:
+  let json = "{\"action\": \"get\", \"node\": { \"createdIndex\": 2," `B.append`
+             "\"key\": \"/message\", \"modifiedIndex\": 2," `B.append`
+             "\"value\": \"Hello world\" }}"
+      expected = Just $ Value "Hello world" :: Maybe (Value Text)
+  in
+    expected @=? A.decode json
+
+canParsePreviousNodeValue :: HU.Test
+canParsePreviousNodeValue = "Can parse previous node value from a response" ~:
+  let json = "{\"action\": \"set\", \"node\": { \"createdIndex\": 3," `B.append`
+             "\"key\": \"/message\", \"modifiedIndex\": 3," `B.append`
+             "\"value\": \"Hello etcd\"}, \"prevNode\": {" `B.append`
+             "\"createdIndex\": 2, \"key\": \"/message\"," `B.append`
+             "\"value\": \"Hello world\", \"modifiedIndex\": 2}}"
+      expected = Just $ PreviousValue "Hello world" :: Maybe (PreviousValue Text)
+  in
+    expected @=? A.decode json
+
+canExtractPairsOfJSONValues :: HU.Test
+canExtractPairsOfJSONValues = "Can extract pairs of JSON objects" ~:
+  let json = "{\"action\": \"set\", \"node\": { \"createdIndex\": 3," `B.append`
+             "\"key\": \"/message\", \"modifiedIndex\": 3," `B.append`
+             "\"value\": \"Hello etcd\"}, \"prevNode\": {" `B.append`
+             "\"createdIndex\": 2, \"key\": \"/message\"," `B.append`
+             "\"value\": \"Hello world\", \"modifiedIndex\": 2}}"
+      expected = Just $ Pair (Value "Hello etcd") (PreviousValue "Hello world")
+        :: Maybe (Pair (Value Text) (PreviousValue Text))
+  in
+    expected @=? A.decode json
+  
